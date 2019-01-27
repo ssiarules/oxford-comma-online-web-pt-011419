1803a4bc81dfd1dcf26b37a192d105c96f350757
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-27 20:01:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,13 @@
 def oxford_comma(array)
-  "kiwi"
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last = array.pop
+    string = array.join(", ") + ", and " + last
+    string
+  end
 end
 
 
